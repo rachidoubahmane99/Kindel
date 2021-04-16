64ecc881f89f39509f2b35fee62f255e39110d72
rachidoubahmane99 <rachidoubahman99@gmail.com>
2021-04-16 15:14:30 +0000

    added search by title

--- KindelConnect/src/main/java/Kindle.java
+++ KindelConnect/src/main/java/Kindle.java
@@ -31,6 +31,7 @@ import javafx.scene.web.WebView;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
+import model.Document;
 import model.Livre;
 
 /*
@@ -174,8 +175,55 @@ public class Kindle extends JFrame {
                     break;
                 case 2:
                     //learn(entree,sortie);
-                     System.out.println("Learn choised");
-                    break;
+                      System.out.println(" Veuillez saisir le titre : ");  
+                        String titre   = (new Scanner(System.in)).nextLine();;
+                        sortie.write("titre\n");
+                        sortie.write(titre+'\n');
+                        sortie.flush(); 
+                        
+                        System.out.println("Veuillez choisir un document à lire :");
+                 
+                   
+                        Kindle compte3 = new Kindle();
+                        LinkedList<Livre> documents = compte3.getLivreByTitre(titre);  
+                           int nb = 0 ;
+                           for(int i = 0 ; i < documents.size();i++){ 
+                             System.out.println(i+" :"+documents.get(i).getIsbn()+"by : editeur:"+documents.get(i).getEditeur());
+                             nb++;
+                           }  
+                            if (nb==0){
+                                System.out.println("aucune document trouvé avec ce titre");      
+                            }
+                            else{
+                            
+                           int livre = (new Scanner(System.in)).nextInt();
+                                  
+                                  
+                                    if(livre < nb && livre> -1){ 
+                                    //affichage du document
+                     
+                            String pdf = documents.get(livre).getUrl();
+                            
+                            Platform.runLater(new Runnable() {
+                            @Override
+                            public void run()
+                             {
+                                  WebEngine engine;
+                                  WebView wv=new WebView();
+                                  engine=wv.getEngine();
+                                  fxpanel.setScene(new Scene(wv));
+                                  engine.load(pdf);
+                                 }
+                             });
+                               frame.setVisible(true);
+                                    }
+                                    else 
+                                        System.out.println("choix invalid");
+            
+                                   
+                            }
+     
+                        break;
                 case 3:
                     //quit(entree,sortie); 
                      System.out.println("leave choised");
@@ -238,6 +286,29 @@ public class Kindle extends JFrame {
            return l;
        }
         return null;
+    }
+    
+    public LinkedList<Livre> getLivreByTitre(String titre) throws SQLException{
+     con = DbConnection.getConnection();
+                   stmt = con.createStatement();
+        String query="select * from livre where titre like '"+titre+"' ";
+        ResultSet rs=stmt.executeQuery(query);
+        
+        LinkedList<Livre> livres= new   LinkedList<Livre> ();
+        
+        while (rs.next()) {
+           String isbn=rs.getString("isbn");
+           String editeur=rs.getString("editeur");
+           String auteur=rs.getString("auteur");
+           int edition=rs.getInt("edition");
+           int nbpages=rs.getInt("nbpages");
+           String auteurs[]={auteur};
+           String url=rs.getString("url");
+           Livre l = new Livre(titre, editeur, edition, isbn,auteurs,url,nbpages);
+           livres.add(l);
+       }
+
+       return livres;
     }
    
 }
